b73021482040d226391d88d5dc4ae03b5fe169d3
fylip <fymor6@gmail.com>
2023-09-29 18:15:19 -0300

    feat: create project to study python POO

---
+++ conta.py
@@ -0,0 +1,49 @@
+class Conta:
+  
+  def __init__(self, numero, titular, saldo, limite):
+    print("Construindo objeto...{}".format(self))
+    self.__numero = numero
+    self.__titular = titular
+    self.__saldo = saldo
+    self.__limite = limite
+    
+  def extrato(self):
+    print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+    
+  def depositar(self, valor):
+    self.__saldo += valor
+    
+  def __pode_sacar(self, valor_a_sacar):
+    valor_disponivel_a_sacar =  self.__saldo + self.__limete
+    return valor_a_sacar <= valor_disponivel_a_sacar
+  
+  def saque(self, valor):
+    if(self.__pode_sacar(valor)):
+      self.__saldo -= valor
+    else:
+      print(f"O valor {valor} passou o limite")      
+    self.__saldo -= valor
+  
+  def transfere(self, valor, destino):
+    self.saque(valor)
+    destino.depositar(valor)
+  
+  @property
+  def saldo(self):
+    return self.__saldo
+  @property
+  def titular(self):
+    return self.__titular
+  @property
+  def limite(self):
+    return self.__limite
+  
+  @limite.setter
+  def limite(self, limite):
+    self.__limite = limite
+  @staticmethod
+  def codigo_banco():
+    return "001"
+  @staticmethod
+  def codigos_bancos():
+    return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
